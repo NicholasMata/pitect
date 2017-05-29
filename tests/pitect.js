58ca3af3965c3b2bfb40b04e777ee6f910a0e689
Nicholas Mata <NicholasMata94@gmail.com>
2017-05-29 07:19:50 -0700

    Fixed tests for travis

--- tests/pitect.js
+++ tests/pitect.js
@@ -2,18 +2,7 @@ var pitect = require('../');
 var expect = require("chai").expect;
 var fs = require('fs');
 
-if (fs.existsSync('/proc/cpuinfo')) {
-    describe("Raspberry Pi Detection", function () {
-        it("Is Raspberry Pi", function () {
-            var isPi = pitect.isPi();
-            expect(isPi).to.equal(true);
-        });
-        it("Model Detection", function () {
-            var model = pitect.piModel();
-            expect(model).to.not.equal(pitect.MODELS.Unknown);
-        });
-    });
-} else {
+if (!fs.existsSync('/proc/cpuinfo')) {
     describe("Raspberry Pi Detection", function () {
         it("Is Raspberry Pi", function () {
             var isPi = pitect.isPi();
