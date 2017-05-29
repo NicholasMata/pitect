508b2cbd78c309f157568cfec66aefa93aa16096
Nicholas Mata <NicholasMata94@gmail.com>
2017-05-29 05:00:04 -0700

    Added functionality for detecting a Raspberry PI. Also for detecting specific Raspberry PI Models.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pitect
+
 NodeJS module used for detecting Raspberry Pi.
+
+## Install
+
+`npm install pitect --save`
+
+## Usage
+
+```js
+const pitect = require('pitect');
+
+if(pitect.isPi()) {
+    console.log('This is a Raspberry Pi.');
+    console.log('This is: ' + pitect.piModel().name);
+    if (pitect.piModel() == pitect.MODELS.PI_A) {
+        console.log('This is a Raspberry PI Model A');
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,170 @@
+var fs = require('fs');
+
+function getCpuInfo(property) {
+  var cpuInfo;
+  try {
+    cpuInfo = fs.readFileSync('/proc/cpuinfo', { encoding: 'utf8' });
+  } catch (e) {
+    // if this fails, this is probably not a pi
+    return null;
+  }
+
+  var propertyString = cpuInfo
+    .split('\n')
+    .map(line => line.replace(/\t/g, ''))
+    .filter(line => line.length > 0)
+    .map(line => line.split(':'))
+    .map(pair => pair.map(entry => entry.trim()))
+    .filter(pair => pair[0] === property)
+
+  if (!propertyString || propertyString.length == 0) {
+    return null;
+  }
+
+  return propertyString[0][1];
+}
+
+function getModel(revision) {
+  for (var modelname in exports.PIMODEL) {
+    // sz will be the names of the objects in SIZE, so
+    // 'SMALL', 'MEDIUM', 'LARGE', 'EXTRALARGE'
+    var model = exports.PIMODEL[modelname]; // Get the object mapped to the name in sz
+    for (var modelRev in model.revisions) {
+        if (modelRev == revision) return model;
+    }
+  }
+  return exports.PIMODEL.Unknown;
+}
+
+exports.isPi = function () {
+  var revision = getCpuInfo('Revision');
+  if (revision == null) return false;
+  return PI_REVISION_NO.indexOf(model) > -1;
+}
+
+exports.piModel = function () {
+  var revision = getCpuInfo('Revision');
+  if (revision == null) return exports.PIMODEL.Unknown;
+  return getModel(revision);
+}
+
+exports.MODELS = {
+  Unknown: {
+    name: 'Unknown'
+  },
+  ////////////////////
+  // Compute Module //
+  ////////////////////
+  Compute_Module: {
+    revisions: [
+      '0011',
+      '0014',
+    ],
+    name: 'Compute Module'
+  },
+
+  /////////////////
+  // PI ORIGINAL //
+  /////////////////
+
+  // Pi Model A
+  PI_A: { revisions: ['0007', '0008', '0009'], name: 'PI Model A' },
+  // Pi Model A+
+  PI_Aplus: {
+    revisions: [
+      '0012',
+      '0015', // (Embest, China) 256 & 512MB
+    ],
+    name : 'PI Model A+'
+  },
+  // Pi Model B Rev 1
+  PI_BRev1: {
+    revisions: [
+      '0002',
+      '0003', // ECN0001 (no fuses, D14 removed)
+    ],
+    name: 'PI Model B Rev 1'
+  },
+  // PI Model B Rev 2
+  PI_BRev2: {
+    revisions: [
+      //256MB
+      '0004',
+      '0005',
+      '0006',
+      //512MB
+      '000d',
+      '000e',
+      '000f'
+    ],
+    name: 'PI Model B Rev 2'
+  },
+  // PI Model B+
+  PI_Bplus: {
+    revisions: [
+      '0010',
+      '0013',
+      '900032'
+    ],
+    name: 'PI Model B+'
+  },
+
+  //////////
+  // PI 2 //
+  //////////
+
+  // PI 2 Model B V1.1
+  PI_2_B_V12: {
+    revisions: [
+      'a01041', // (Sony, UK)
+      'a21041' // (Embest, China)
+    ],
+    name: 'PI 2 Model B v1.1'
+  },
+  // PI 2 Model B V1.2
+  PI_2_B_V12: {
+    revisions: [
+      'a22042'
+    ],
+    name: 'PI 2 Model B v1.2'
+  },
+
+  /////////////
+  // PI Zero //
+  /////////////
+
+  // PI Zero V1.2
+  PI_Z_V12: {
+    revisions: [
+      '900092'
+    ],
+    name: 'PI Zero v1.2'
+  },
+  // PI Zero V1.3
+  PI_Z_V13: {
+    revisions: [
+      '900093'
+    ],
+    name: 'PI Zero v1.3'
+  },
+  // PI Zero W
+  PI_Z_W: {
+    revisions: [
+      "0x9000C1"
+    ],
+    name: 'PI Zero W'
+  },
+
+  //////////
+  // PI 3 //
+  //////////
+
+  // PI 3 Model B
+  PI_3_B: {
+    revisions: [
+      'a02082', // (Sony, UK)
+      'a22082' // (Embest, China)
+    ],
+    name: 'PI 3 Model B'
+  }
+}
